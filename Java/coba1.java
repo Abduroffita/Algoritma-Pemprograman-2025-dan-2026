9bd28e0dd914c8721f3033e53367437fdaf23ada
Abduroffita <usamahalqorni@gmail.com>
2025-11-18 11:22:35 +0700

    pertemuan ke 4

--- Java/coba1.java
+++ Java/coba1.java
@@ -1,7 +1,8 @@
 class coba1 {
-public static void main(String[] args) { String nim = "I.110186";
-String nama = "Muhammad Encep"; String umur = "35";
-String profesi = "Dosen";
+public static void main(String[] args) { String nim = "I.2510736";
+String nama = "Abduroffi Thoriq Alfaruq"; 
+String umur = "19";
+String profesi = "Mahasiswa";
 
 System.out.println(nim); System.out.println(nama); System.out.println(umur); System.out.println(profesi);
 }
