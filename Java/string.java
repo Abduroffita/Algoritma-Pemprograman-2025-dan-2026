092f9c19b14fcdfb9784f5e53393d13159b42e16
Abduroffita <usamahalqorni@gmail.com>
2025-11-21 21:00:45 +0700

    Delete Java/string.java

--- Java/string.java
+++
@@ -1,6 +0,0 @@
-public class string {
-    public static void main(String[] args) {
-        String halo = "halo, Selamat datang di Program Studi Ilmu Komputer!!!";
-        System.out.println(halo);
-    }
-}
