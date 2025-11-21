06ebc339cb2193d7c808aea872e258edc1a5875f
Abduroffita <usamahalqorni@gmail.com>
2025-11-21 16:30:43 +0700

    Praktikum CPP dari Pertemuan 4-5

---
+++ C++/pertemuan 4-5/latihan1_p5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nama = "Abduroffi Thoriq Alfaruq";
+    int umur = 19;
+    float tinggi = 169;
+    bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Aktif: " << aktif << endl;
+    return 0;
+}
