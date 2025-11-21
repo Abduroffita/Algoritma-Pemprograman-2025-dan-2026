b9f29593fbbcaa9df1b482fb7dc7abf85857f5e6
Abduroffita <usamahalqorni@gmail.com>
2025-11-21 16:34:19 +0700

    praktikum python dari pertemuan 4-7

---
+++ python/pertemuan_6/tipedata.py
@@ -0,0 +1,37 @@
+print('---Belajar Tipe Data---')
+
+#tipe data integers x = 5
+y = 3
+z = 5+3
+print(z)
+
+#tipe data floating numbers
+a=8.5
+print=(a)
+
+#Menggunakan single quote
+myfirstName = "Abduroffi"
+
+#menggunakan double quote
+mylastname = "Alfaruq"
+
+#Penulisan angka sebagai string
+myAge = "19"
+
+#Penulisan string multiline
+myHobby = """Hobi Saya adalah memasak."""
+
+#Mencetak 2 variabel string dengan perintah print
+print(myfirstName + "" + mylastname)
+print("Umur saya" + myAge)
+print("Hobby saya" + myHobby)
+
+#Contoh penulisan Boolean yang benar
+a=True
+b=False
+
+#contoh penulisan boolean yang salah
+#jika dijalankan, pada baris ini akan error
+c=a
+
+print(c)
