1a0079c9982a29685fc7e9f82bbe368d95f97ad9
Abduroffita <usamahalqorni@gmail.com>
2025-11-21 20:59:18 +0700

    tugas praktikum CPP

---
+++ C++/Pertemuan 4-5/tugas-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nama = "Bayu";
+    int umur = 19;
+    float tinggi = 170.5;
+    bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Aktif: " << aktif << endl;
+    return 0;
+}
