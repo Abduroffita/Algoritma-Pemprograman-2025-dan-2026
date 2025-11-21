693270c83f50607902b4ecbd10dcc532162da684
Abduroffita <usamahalqorni@gmail.com>
2025-11-21 16:32:59 +0700

    praktikum php dari pertemuan 4-7

---
+++ php/pertemuan 4-5/coba2.php
@@ -0,0 +1,8 @@
+<?php
+$gaji = 50000;
+$pajak = 2.5;
+$thp = $gaji - ($gaji * $pajak);
+
+echo "Gaji sebelum pajak = Rp. $gaji <br>"; 
+echo "Gaji Bersih = Rp. $thp";
+?>
---
+++ php/pertemuan_6/Variabel.php
@@ -0,0 +1,15 @@
+<?php
+$char ='A';
+$string = "Abduroffi Thoriq Alfaruq";
+$umur = 19;
+$penghasilan = 50000;
+$nilai_uts= 85.25;
+
+echo "---Belajar Variabel---<br><br>";
+echo "Variabel Char :".$char."<br>";
+echo "Variabel String : $string<br>";
+echo "variabel umur : $$umur<br>";
+echo "variabel penghasilan : $penghasilan<br>";
+echo "variabel nilai_uts : $nilai_uts<br>";
+?>
+
