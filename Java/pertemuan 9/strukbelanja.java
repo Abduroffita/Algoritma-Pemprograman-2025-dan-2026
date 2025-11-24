de9280bed5a6ba1b2fde95bb5c59a839dddbe3a1
Abduroffita <usamahalqorni@gmail.com>
2025-11-24 12:47:20 +0700

    pertemuan 9

--- Java/pertemuan 9/strukbelanja.java
+++ Java/pertemuan 9/strukbelanja.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StrukBelanja {
+public class strukbelanja {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -12,7 +12,7 @@ public class StrukBelanja {
         int jumlah = input.nextInt();
 
         float total = harga * jumlah;
-        float diskon = (total > 100000) ? total * 0.10f : 0;
+        float diskon = (total > 73600) ? total * 0.6f : 0;
         float totalBayar = total - diskon;
 
         System.out.println("\n===== STRUK PEMBELIAN =====");
