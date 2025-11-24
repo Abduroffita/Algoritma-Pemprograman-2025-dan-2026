1ded75c92f854e92f1e7d4bc89ee55747adcc56f
Abduroffita <usamahalqorni@gmail.com>
2025-11-24 11:12:18 +0700

    pertemuan 9 input output

---
+++ php/pertemuan 9/strukbelanja.php
@@ -0,0 +1,25 @@
+$<?php
+
+$nama = readline("Masukan nama barang: ");
+$harga = readline("Masukan harga barang: ");
+$jumlah = readline("Masukan jumlah beli: ");
+
+$total = $harga * $jumlah;
+
+if ($total > 73600) {
+    $diskon = $total * 0.6;
+} else {
+    $diskon = 0;
+}
+
+$total_bayar = $total - $diskon;
+
+echo "\n===== STRUK PEMBELIAN =====\n";
+echo "Nama Barang   : $nama\n";
+echo "Harga Satuan  : $harga\n";
+echo "Jumlah Beli   : $jumlah\n";
+echo "Total Harga   : $total\n";
+echo "Diskon        : $diskon\n";
+echo "Total Bayar   : $total_bayar\n";
+
+?>
